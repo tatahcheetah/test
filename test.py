633e8d254b54df2582e436da27891003cc84b696
Ratana Ariyaburoos <ar.ratana_st@tni.ac.th>
2017-02-02 18:04:56 +0000

    third commit

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from random import randint
+
 print("Be careful not to fall off!")
 
 i=0
@@ -6,3 +8,9 @@ for i in range(5) :
         print("Now THREE!!!")
     i+=1
 print(i)
+
+for i in range(10):
+    if i>7:
+        print(i,randint(0,10))
+    i+=1
+    
